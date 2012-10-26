992eae921c7a0b6700ba390092c3e2491780c327
Ole Weidner <ole.weidner@rutgers.edu>
2012-10-26 16:57:05 -0400

    added command-line arguments

--- scripts/air-run-bfast-RANGER.sh
+++ scripts/air-run-bfast-RANGER.sh
@@ -1,9 +1,17 @@
 #!/bin/bash  
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Usage: air-run... APPKEY"
+
+export RUN=RANGER_BFAST_$1
+echo "Registering application run as: $RUN"
+
 module load python
 . $SCRATCH/software/pythonenv/bin/activate
 export BFAST_DIR=$SCRATCH/software/bfast/
-
-export RUN=RANGER_bfast_fewwd_bj128_ref3G_read170M
 
 air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg2122/hg_2122.fa > /dev/null
--- scripts/air-run-bowtie-RANGER.sh
+++ scripts/air-run-bowtie-RANGER.sh
@@ -1,9 +1,17 @@
 #!/bin/bash  
 
+die () {
+    echo >&2 "$@"
+    exit 1
+}
+
+[ "$#" -eq 1 ] || die "Usage: air-run... APPKEY"
+
+export RUN=RANGER_BOWTIE_$1
+echo "Registering application run as: $RUN"
+
 module load python
 . $SCRATCH/software/pythonenv/bin/activate
 export BOWTIE_DIR=$SCRATCH/software/bowtie2/
-
-export RUN=RANGER_bowtie-Marisa_miRNA-1_qseq.fq
 
 air-run $RUN $BOWTIE_DIR/bin/bowtie2 --time -x $BOWTIE_DIR/data/ref/hg18/hg18 -U $BOWTIE_DIR/data/reads/Marisa_miRNA-1_qseq.fq -S result.sam 
