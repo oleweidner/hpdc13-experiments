d1c8d96bffe17f7e512b68ab2c9cda52d30e75c5
Ole Weidner <oweidner@login1.uc.futuregrid.org>
2012-10-26 14:57:34 -0500

    fixed bfast paths

--- scripts/air-run-bfast-INDIA.sh
+++ scripts/air-run-bfast-INDIA.sh
@@ -6,4 +6,4 @@ export BFAST_DIR=$HOME/software/bfast/
 
 export RUN=INDIA_bfast_fewwd_bj128_ref3G_read170M
 
-air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg_2122.fa > /dev/null
+air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg2122/hg_2122.fa > /dev/null
