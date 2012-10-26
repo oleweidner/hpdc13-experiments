8c59759cf3d829b17b7b0dd6348fc7b5159a98d3
Ole Weidner <oweidner@login2.uc.futuregrid.org>
2012-10-25 21:12:05 -0500

    added wrapper scripts for hotel

--- scripts/air-run-bfast-HOTEL.sh
+++ scripts/air-run-bfast-HOTEL.sh
@@ -4,6 +4,6 @@ module load python
 . $HOME/software/pythonenv/bin/activate
 export BFAST_DIR=/N/u/oweidner/software/bfast/
 
-export RUN=INDIA_bfast_fewwd_bj128_ref3G_read170M
+export RUN=HOTEL_bfast_fewwd_bj128_ref3G_read170M
 
 air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg_2122.fa > /dev/null
--- scripts/air-run-bowtie-HOTEL.sh
+++ scripts/air-run-bowtie-HOTEL.sh
@@ -4,6 +4,6 @@ module load python
 . $HOME/software/pythonenv/bin/activate
 export BOWTIE_DIR=/N/u/oweidner/software/bowtie2/
 
-export RUN=INDIA_bowtie-Marisa_miRNA-1_qseq.fq
+export RUN=HOTEL_bowtie-Marisa_miRNA-1_qseq.fq
 
 air-run $RUN $BOWTIE_DIR/bin/bowtie2 --time -x $BOWTIE_DIR/data/ref/hg18/hg18 -U $BOWTIE_DIR/data/reads/Marisa_miRNA-1_qseq.fq -S result.sam 
