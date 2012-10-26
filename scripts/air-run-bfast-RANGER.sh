714d24b83089fe772830e1786999dd4618434e70
oweidner <ole.weidner@me.com>
2012-10-26 15:02:55 -0500

    fixed paths

--- scripts/air-run-bfast-RANGER.sh
+++ scripts/air-run-bfast-RANGER.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 
 module load python
-. $HOME/software/pythonenv/bin/activate
-export BFAST_DIR=$HOME/software/bfast/
+. $SCRATCH/software/pythonenv/bin/activate
+export BFAST_DIR=$SCRATCH/software/bfast/
 
-export RUN=INDIA_bfast_fewwd_bj128_ref3G_read170M
+export RUN=RANGER_bfast_fewwd_bj128_ref3G_read170M
 
-air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg_2122.fa > /dev/null
+air-run $RUN $BFAST_DIR/bin/bfast match -A 1 -r $BFAST_DIR/data/reads/read.170M.fastq -f $BFAST_DIR/data/reference/hg2122/hg_2122.fa > /dev/null
--- scripts/air-run-bowtie-RANGER.sh
+++ scripts/air-run-bowtie-RANGER.sh
@@ -1,9 +1,9 @@
 #!/bin/bash  
 
 module load python
-. $HOME/software/pythonenv/bin/activate
-export BOWTIE_DIR=$HOME/software/bowtie2/
+. $SCRATCH/software/pythonenv/bin/activate
+export BOWTIE_DIR=$SCRATCH/software/bowtie2/
 
-export RUN=INDIA_bowtie-Marisa_miRNA-1_qseq.fq
+export RUN=RANGER_bowtie-Marisa_miRNA-1_qseq.fq
 
 air-run $RUN $BOWTIE_DIR/bin/bowtie2 --time -x $BOWTIE_DIR/data/ref/hg18/hg18 -U $BOWTIE_DIR/data/reads/Marisa_miRNA-1_qseq.fq -S result.sam 
