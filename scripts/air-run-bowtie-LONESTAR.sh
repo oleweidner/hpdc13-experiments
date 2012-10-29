4d8b26f85fcd58f3c1442011ff7d8e07e6325b90
Ole Weidner <tg802352@login2.ls4.tacc.utexas.edu>
2012-10-29 14:44:30 -0500

    switched from ranger to lonestar

--- scripts/air-run-bowtie-LONESTAR.sh
+++ scripts/air-run-bowtie-LONESTAR.sh
@@ -7,11 +7,11 @@ die () {
 
 [ "$#" -eq 1 ] || die "Usage: air-run... APPKEY"
 
-export RUN=RANGER_BOWTIE_$1
+export RUN=LONESTAR_BOWTIE_$1
 echo "Registering application run as: $RUN"
 
 module load python
-. $SCRATCH/software/pythonenv/bin/activate
-export BOWTIE_DIR=$SCRATCH/software/bowtie2/
+. $WORK/software/pythonenv/bin/activate
+export BOWTIE_DIR=$WORK/software/bowtie2/
 
 air-run $RUN $BOWTIE_DIR/bin/bowtie2 --time -x $BOWTIE_DIR/data/ref/hg18/hg18 -U $BOWTIE_DIR/data/reads/Marisa_miRNA-1_qseq.fq -S result.sam 
